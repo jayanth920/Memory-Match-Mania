fe9518f8bcd7b85532bf9170a4eff66c40ed0f93
jayanth920 <jayanthgajula92004@gmail.com>
2022-10-22 11:42:05 -0500

    Added little more images,wrote starter js code log for images,time,moves logic

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//DECLARATIONS
 const moves = document.getElementById("moves-count");
 const timeValue = document.getElementById("time")
 const startButton = document.getElementById("start")
@@ -10,7 +11,50 @@ let interval
 let firstCard = false
 let secondCArd = false
 
-//Pictures Array
+//PICTURES ARRAY
 const items = [
-    {name:"pizza",image:"images/pizza.jpg"}
+    {name:"pizza",image:"images/pizza.jpeg"},
+    {name:"hotdog",image:"images/hotdog.jpeg"},
+    {name:"burrito",image:"images/burrito.jpeg"},
+    {name:"tacos",image:"images/tacosjpeg"},
+    {name:"burger",image:"images/burger.jpeg"},
+    {name:"sandwich",image:"images/sandwich.jpeg"},
+    {name:"fries",image:"images/fries.jpeg"},
+    {name:"strawberry",image:"images/strawberry.jpeg"},
+    {name:"watermelon",image:"images/watermelon.jpeg"},
+    {name:"apple",image:"images/apple.jpeg"},
+    {name:"coconut",image:"images/coconut.jpeg"},
+    {name:"orange",image:"images/orange.jpeg"}
 ]
+
+//DEFAULT INITIAL TIME
+let seconds = 0      
+let minutes = 0
+
+//DEFAULT INITIAL MOVE and WIN 
+let movesCount = 0
+let wincCount = 0
+
+//TIMER
+const timeRunner = () => {
+    seconds += 1
+    if (seconds >= 60){     //SECONDS TO MINUTES
+    minutes += 1
+    seconds = 0
+}
+
+//DISPLAY FORMAT FOR TIME
+let secondsValue = seconds < 10 ? `0${seconds}` : seconds
+let minutesValue = minutes < 10 ? `0${minutes}` : minutes
+timeValue.innerHTML = `<span>Time:</span>${minutesValue}:${secondsValue}`
+
+}
+
+//MOVES COUNTER
+const movesCounter = () => {
+    movesCount +=1;
+    moves.innetHTML = `<span>Moves:</span>${movesCount}`
+
+}
+
+
