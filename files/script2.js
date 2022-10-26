70cd610a7933572eaee6a6eeaf372cdc3d672ad5
jayanth920 <jayanthgajula92004@gmail.com>
2022-10-26 10:07:18 -0500

    updated pics:

--- files/script2.js
+++ files/script2.js
@@ -82,7 +82,7 @@ const generateGame = () => {
         throw new Error("The dimension of the board must be an even number.")
     }
 
-    const emojis = ['🍕', '🍒', '🍓', '🌮', '🌯', '🍇', '🍉', '🍟', '🍎', '🌭']
+    const emojis = ['🍕', '🥘', '🐻', '🐻‍❄️', '🧠', '🦊', '🍉', '🍟', '🌳', '🍄']
     const picks = pickRandom(emojis, (dimensions * dimensions) / 2) 
     const items = shuffle([...picks, ...picks])
     const cards = `
